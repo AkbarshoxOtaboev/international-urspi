fad31aa791bb75354cb07cca9586936e9d9335e1
Admin <otaboevakbar96@gmail.com>
2024-10-05 14:46:47 +0500

    add language pack

---
+++ src/main/java/com/example/xalqaro/AdminController.java
@@ -0,0 +1,43 @@
+package com.example.xalqaro;
+
+import com.example.xalqaro.students.Student;
+import com.example.xalqaro.students.StudentService;
+import com.example.xalqaro.user.User;
+import com.example.xalqaro.user.UserRepository;
+import com.example.xalqaro.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("")
+@RequiredArgsConstructor
+public class AdminController {
+    private final UserService userService;
+    private final StudentService studentService;
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model) {
+        User user =  userService.getCurrentUser();
+        model.addAttribute("user", user);
+        return "admin";
+    }
+
+    @GetMapping("/admin/students")
+    public String students(Model model) {
+        User user =  userService.getCurrentUser();
+        model.addAttribute("user", user);
+        List<Student> students = studentService.getStudents();
+        model.addAttribute("students", students);
+        return "students";
+    }
+}
--- src/main/java/com/example/xalqaro/students/StudentController.java
+++ src/main/java/com/example/xalqaro/students/StudentController.java
@@ -1,5 +1,7 @@
 package com.example.xalqaro.students;
 
+import com.example.xalqaro.user.User;
+import com.example.xalqaro.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class StudentController {
 
     private final StudentService studentService;
+    private final UserService userService;
 
     @GetMapping("/")
     public String index(Model model) {
